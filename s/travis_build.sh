482bf17710d86af71455c4e0974c9da121b4ffb5
sjs2109 <8577129+sjs2109@users.noreply.github.com>
2019-05-05 23:44:50 +0900

    Update travis_build.sh

--- s/travis_build.sh
+++ s/travis_build.sh
@@ -7,7 +7,7 @@ build()
     go build -o blog
     ./blog -deploy
     ./netlifyctl -A $NETLIFY_TOKEN deploy || true
-    cat netlifyctl-debug.log || true
+    #cat netlifyctl-debug.log || true
 }
 
 setup_git()
